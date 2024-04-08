900ca3739e33afa808b56c1e0c62d9d6c9ee19b0
dalgudot <dalgudot@gmail.com>
2024-04-08 18:00:31 +0900

    remove button & code

--- apps/scheduler/src/app/[locale]/page.tsx
+++ apps/scheduler/src/app/[locale]/page.tsx
@@ -1,8 +1,6 @@
 import { useTranslations } from 'next-intl';
 import Link from 'next/link';
 import { Locale } from '@/i18n';
-import { Code } from '@repo/ui/code';
-import { Button } from '@repo/ui/button';
 
 export default function Home({
   params: { locale },
@@ -14,10 +12,6 @@ export default function Home({
   return (
     <main>
       <h1>{t('Small App Project')} </h1>
-      <Code> This is a code!</Code>
-      <Button className='' appName='Scheduler'>
-        This is a Button!
-      </Button>
       <Link href={`/${locale}/blog`}>Blog</Link>
     </main>
   );
