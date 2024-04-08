ab28104ce7c933ce0aaad99d5fa8d16b99d78e74
dalgudot <dalgudot@gmail.com>
2024-04-08 17:56:48 +0900

    add Button test

--- apps/scheduler/src/app/[locale]/page.tsx
+++ apps/scheduler/src/app/[locale]/page.tsx
@@ -2,6 +2,7 @@ import { useTranslations } from 'next-intl';
 import Link from 'next/link';
 import { Locale } from '@/i18n';
 import { Code } from '@repo/ui/code';
+import { Button } from '@repo/ui/button';
 
 export default function Home({
   params: { locale },
@@ -14,6 +15,9 @@ export default function Home({
     <main>
       <h1>{t('Small App Project')} </h1>
       <Code> This is a code!</Code>
+      <Button className='' appName='Scheduler'>
+        This is a Button!
+      </Button>
       <Link href={`/${locale}/blog`}>Blog</Link>
     </main>
   );
