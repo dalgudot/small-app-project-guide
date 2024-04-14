92c8ce572c463d37d6ab1c3252b31e6c1eb91f72
dalgudot <dalgudot@gmail.com>
2024-04-14 13:43:31 +0900

    add ssg

--- apps/guide/src/app/[locale]/[category]/[title]/page.tsx
+++ apps/guide/src/app/[locale]/[category]/[title]/page.tsx
@@ -1,24 +1,41 @@
-import { Locale } from '@/i18n';
+import { Locale, locales } from '@/i18n';
 import dynamic from 'next/dynamic';
+import fs from 'fs';
 
-// ✨ SSG - 모든 파일 주소 미리 받아와 각 MDXContent를 모두 가져와야 SSG 가능!
-// ```pnpm build``` 로 확인 가능
-// https://nextjs.org/docs/app/building-your-application/routing/dynamic-routes#generating-static-params
-// export const generateStaticParams = async ({
-//   params,
-// }: Readonly<{
-//   params: { locale: Locale; category: string; title: string };
-// }>) => {
-//   const { locale, category, title } = params;
+interface Param {
+  locale: Locale;
+  category: string;
+  title: string;
+}
 
-//   return params;
+export async function generateStaticParams() {
+  let params: Param[] = [];
 
-// const posts = getPostListData(locale);
+  // ✨ 한, 영으로 모든 콘텐츠를 작성하지 않으면서 SSG
+  locales.forEach((locale) => {
+    const categories = fs.readdirSync(`src/posts/${locale}`);
 
-// return posts.map((post) => ({
-//   slug: post.slug,
-// }));
-// };
+    categories.forEach((category) => {
+      const files: string[] = fs
+        .readdirSync(`src/posts/${locale}/${category}`)
+        .filter((file) => file.endsWith('.meta.mdx'));
+
+      files.forEach((file) => {
+        const param: Param = {
+          locale: locale,
+          category: category,
+          title: file.replace('.meta.mdx', ''),
+        };
+
+        params.push(param);
+      });
+    });
+  });
+
+  // console.log('params', params);
+
+  return params;
+}
 
 // Dynamic Routes
 // https://nextjs.org/docs/app/building-your-application/routing/dynamic-routes
@@ -31,7 +48,6 @@ export default function PostPage({
 
   // MDX 활용 위한 Dynamic import
   // https://nextjs.org/docs/app/building-your-application/optimizing/lazy-loading
-  // ✨ 한, 영으로 모든 콘텐츠를 작성하지 않기 위한 Custom Logic
   const MDXContent = dynamic(
     () => import(`../../../../posts/${locale}/${category}/${title}.mdx`)
   );
