3beaa3fa9e9ca8c9ff5adcd18e5318db0fbcc442
dalgudot <dalgudot@gmail.com>
2024-04-29 09:21:51 +0900

    update message

--- packages/ui/src/components/MDX/SchedulerAddress/SchedulerAddress.tsx
+++ packages/ui/src/components/MDX/SchedulerAddress/SchedulerAddress.tsx
@@ -5,8 +5,10 @@ import { SchedulerLogoSVG } from '../../../Foundations/Logo/SchedulerLogoSVG';
 export function SchedulerAddress({ locale }: { locale: 'ko' | 'en' }) {
   const name =
     locale === 'ko'
-      ? '스케줄러 · 한국인이 만든 아이폰 달력(캘린더) 앱'
-      : 'Scheduler · iPhone Calendar App with Essential Features';
+      ? // ? '스케줄러 · 한국인이 만든 아이폰 달력(캘린더) 앱'
+        '스케줄러 · 미리 알림부터 캘린더까지. 쉽게 연동하는 아이폰 달력 앱' // 2024.4.29 메시지 변경
+      : // : 'Scheduler · iPhone Calendar App with Essential Features';
+        'Scheduler · Seamlessly Link Reminders and Calendars. iPhone Calendar App.'; // 2024.4.29 메시지 변경
 
   const href =
     locale === 'ko'
